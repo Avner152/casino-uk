b6e1136e9fd8c572c86645399414724c7cee1626
Avner Levy <AvnerX92@gmail.com>
2024-07-01 23:40:37 +0300

    sdss

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import Footer from "./components/Footer";
 function App() {
   const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" });
   // const isLaptop = useMediaQuery({ query: "(min-width: 1024px)" });
-  const isTablet = useMediaQuery({
-    query: "(min-width: 768px) and (max-width: 1023px)",
-  });
-  const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
+  // const isTablet = useMediaQuery({
+  //   query: "(min-width: 768px) and (max-width: 1023px)",
+  // });
+  // const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
   return (
     <>
       <div className={`${isDesktop ? "w-50" : "w-100 p-2"} m-auto casino-main`}>
