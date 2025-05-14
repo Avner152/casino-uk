04ee33192241d7bdeee74f6981bf37beadbfec77
Avner Levy <AvnerX92@gmail.com>
2025-05-14 21:52:40 +0300

    p

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -118,7 +118,7 @@ const Intro = observer(() => {
           <div>
             <h1 className={`intro-title fw-bold w-${isDesktop ? 75 : 100}`}>
               {myStore.type === "blanca"
-                ? `Check our UK's Best Casinos Of ${curDate.toLocaleString(
+                ? `Check UK's Best Casinos Of ${curDate.toLocaleString(
                     "default",
                     {
                       month: "long",
