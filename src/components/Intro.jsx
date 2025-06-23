4cd44bb20a6032b714ac0cced41c9317186a70f4
Avner Levy <AvnerX92@gmail.com>
2025-06-23 15:20:57 +0300

    pp

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -97,8 +97,8 @@ const Intro = observer(() => {
   return (
     <div className="intro mt-4 tit-n-des text-white pt-3">
       {page ? (
-        <div className="text-white mb-5 mt-4">
-          <h1 className="intro-title fw-bold w-75">
+        <div className="text-white mb-5_ mt-4">
+          <h1 className="intro-title fw-bold w-75 sm-w-100">
             {headlineInfo[page].title}{" "}
             {location.pathname.includes("games") && fullYear}
           </h1>
