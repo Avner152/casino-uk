5ea693e2d6f49772dfd02a723840e0d3053b087f
Avner Levy <AvnerX92@gmail.com>
2025-12-29 21:08:19 +0200

    market Advertiser Disclosure

--- src/App.js
+++ src/App.js
@@ -194,12 +194,21 @@ const App = observer(() => {
       <div className={`w-${isDesktop ? 60 : 100} m-auto casino-main`}>
         <br />
         {myStore.type.startsWith("bl") && (
-          <p
-            style={{ paddingTop: 80 }}
-            className="text-center text-white pb-0 mb-0 fs-2"
-          >
-            ADVERTORIAL
-          </p>
+          <>
+            <p
+              style={{ paddingTop: 80 }}
+              className="text-center text-white pb-0 mb-0 fs-2"
+            >
+              ADVERTORIAL
+            </p>
+            <aside className="fs-8 text-white lh-1 text-center w-75 sm-w-100 mx-auto px-2">
+              We receive advertising fees from the brands we review, which may
+              influence our rankings and scores. We do not compare every service
+              provider on the market Advertiser Disclosure 18+."T&C" apply - the
+              applicable operator's terms apply for each offer below and free
+              offers may include additional terms.
+            </aside>
+          </>
         )}
         <Intro />
         {/* <MyRoutes captchaToken={captchaToken} /> */}
