614fd85105c46164f97e86b6532beeb1e5ea40e6
Avner Levy <AvnerX92@gmail.com>
2026-01-18 17:38:54 +0200

    p

--- src/components/sport/SportSection.jsx
+++ src/components/sport/SportSection.jsx
@@ -36,6 +36,14 @@ const SportSection = observer(({ captchaToken }) => {
     },
   ];
 
+  // const images = importImages(
+  //   require.context("../assets/logos", false, /\.(png|jpe?g|svg)$/)
+  // );
+
+  // const importedIcons = importImages(
+  //   require.context("../assets/icons", false, /\.(svg)$/)
+  // );
+
   const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" });
   // const isTablet = useMediaQuery({
   //   query: "(min-width: 768px) and (max-width: 1023px)",
