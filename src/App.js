8afd281985360be252fd5954bcea0b0718a8cea6
Avner Levy <AvnerX92@gmail.com>
2026-05-06 23:04:32 +0300

    pp

--- src/App.js
+++ src/App.js
@@ -160,7 +160,7 @@ const App = observer(() => {
                 </div>
                 <Button
                   className="text-uppercase main-btn mb-3"
-                  href={`${casinoItem?.url}${appendQueryParams(searchParams)}`}
+                  href={`${casinoItem?.url}${appendQueryParams(searchParams, 0)}`}
                   target="_blank"
                 >
                   get bonus
