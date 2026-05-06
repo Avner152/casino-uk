5a1364db7585134e793075e312895344f5471206
Avner Levy <AvnerX92@gmail.com>
2026-05-06 22:54:50 +0300

    pp

--- src/json/helpers.js
+++ src/json/helpers.js
@@ -71,7 +71,7 @@ export const pages = {
   ],
 };
 
-export function appendQueryParams(searchParams) {
+export function appendQueryParams(searchParams, index) {
   const list = [
     "msclkid",
     "campaignid",
@@ -98,7 +98,8 @@ export function appendQueryParams(searchParams) {
 
   if (document.referrer) {
     const parent_referer = new URL(document.referrer).hostname;
-    queryParams += `&parent_referer=${parent_referer}`;
+    queryParams += `&sub_id_10=${parent_referer}`;
+    if (index) queryParams += `&sub_id_11=${index}`;
   }
 
   return queryParams;
