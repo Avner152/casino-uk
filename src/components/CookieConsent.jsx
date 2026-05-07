305fb0dee5768e807ad19352050b0f1df57442d0
Avner Levy <AvnerX92@gmail.com>
2026-05-07 18:55:28 +0300

    pp

--- src/components/CookieConsent.jsx
+++ src/components/CookieConsent.jsx
@@ -41,6 +41,12 @@ const CookieConsent = observer(({ setCookieStatus }) => {
         <Button onClick={cookieHandler} className="cookie-accept cookie-button">
           OK, got it
         </Button>
+        <Button
+          onClick={() => setCookieStatus(false)}
+          className="cookie-button"
+        >
+          Cancel
+        </Button>
       </div>
     </div>
   );
