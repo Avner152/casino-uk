a985dd6a993f31ed04ffb953b217b6849a91f8a9
Avner Levy <AvnerX92@gmail.com>
2026-05-17 20:03:52 +0300

    pp

--- src/components/PortalSection.jsx
+++ src/components/PortalSection.jsx
@@ -38,6 +38,7 @@ const PortalSection = observer(({ captchaToken }) => {
         const response = await axios.get("https://api.ipify.org?format=json");
         setUserIp(response.data.ip);
       } catch (err) {
+        setUserIp("0.0.0.0");
         console.error("Error fetching IP:", err);
       }
     };
